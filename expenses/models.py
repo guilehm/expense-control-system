6686376d748b5e67bad21c45830b658794bf6ed4
guile.hm <guile.hm@hotmail.com>
2018-05-08 22:50:46 -0300

    Create tags model

--- expenses/models.py
+++ expenses/models.py
@@ -10,10 +10,49 @@ class Expense(models.Model):
     due_date = models.DateField(db_index=True)
     paid_out = models.BooleanField(default=False)
     note = models.TextField(blank=True, null=True)
+    slug = models.SlugField()
+    category = models.ForeignKey(
+        'expenses.Category',
+        related_name='expenses',
+        on_delete=models.CASCADE,
+    )
+    tags = models.ManyToManyField(
+        'expenses.Tags',
+        on_delete=models.CASCADE,
+        related_name='expenses',
+    )
 
     date_added = models.DateTimeField(auto_now_add=True, db_index=True)
     date_changed = models.DateTimeField(auto_now=True, db_index=True)
 
-# repeat
-# category
-# tags
+    def __str__(self):
+        return self.title
+
+    # repeat
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+
+    date_added = models.DateTimeField(auto_now_add=True, db_index=True)
+    date_changed = models.DateTimeField(auto_now=True, db_index=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+
+class Tags(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+
+    date_added = models.DateTimeField(auto_now_add=True, db_index=True)
+    date_changed = models.DateTimeField(auto_now=True, db_index=True)
+
+    def __str__(self):
+        return self.title
