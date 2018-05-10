c96f385e88e3b3f2812a47988c88082a9b8f734c
guile.hm <guile.hm@hotmail.com>
2018-05-09 21:48:21 -0300

    Create bank account, deposit and cashing

--- core/models.py
+++ core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
-
     date_added = models.DateTimeField(auto_now_add=True, db_index=True)
     date_changed = models.DateTimeField(auto_now=True, db_index=True)
 
@@ -20,9 +20,30 @@ class Tag(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
-
     date_added = models.DateTimeField(auto_now_add=True, db_index=True)
     date_changed = models.DateTimeField(auto_now=True, db_index=True)
 
     def __str__(self):
         return self.title
+
+
+class BankAccount(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    agency = models.CharField(max_length=6, blank=True, null=True)
+    number = models.IntegerField(blank=True, null=True)
+    when_opened = models.DateField()
+
+
+class Deposit(models.Model):
+    account = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
+    total = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    when = models.DateTimeField()
+
+
+class Cashing(models.Model):
+    account = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
+    total = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    when = models.DateTimeField()
