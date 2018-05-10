b240cc4ba20540d35d719c32da6a28b3a7a3baca
guile.hm <guile.hm@hotmail.com>
2018-05-09 23:20:52 -0300

    Create card with bank balance

--- controller/settings.py
+++ controller/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'core',
     'bank',
     'transactions',
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Brazil/East'
 
 USE_I18N = True
 
