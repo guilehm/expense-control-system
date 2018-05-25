0830d602f3a5503c95ac416b1ffd7f39cf300f94
Guilherme Luis <guile.hm@hotmail.com>
2018-05-24 21:03:03 -0300

    Create revenue edit form

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+
 from bank.models import BankAccount
+from transactions.models import Revenue
 
 class BankAccountCreateForm(forms.ModelForm):
     class Meta:
@@ -7,3 +9,9 @@ class BankAccountCreateForm(forms.ModelForm):
         exclude = [
             'owner',
         ]
+
+
+class RevenueEditForm(forms.ModelForm):
+    class Meta:
+        model = Revenue
+        fields = '__all__'
