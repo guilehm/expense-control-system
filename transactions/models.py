77d179f40e3524399a366d7e0f4cbb9d51bd5567
Guilherme Luis <guile.hm@hotmail.com>
2018-06-01 14:01:46 -0300

    Create method for total update

--- transactions/models.py
+++ transactions/models.py
@@ -72,3 +72,8 @@ class Expense(models.Model):
 
     def __str__(self):
         return self.title
+
+    # FIXME: just for testing, improve if works
+    def update_total(self, value):
+        if value >= 0:
+            self.total = value
