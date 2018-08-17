bf84c8733ad2967dc66d5159a11f640a364b6af4
Guilherme Luis <guile.hm@hotmail.com>
2018-08-16 23:41:09 -0300

    Remove import unused

--- controller/urls.py
+++ controller/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.documentation import include_docs_urls
