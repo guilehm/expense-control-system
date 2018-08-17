80726ee272ca6b6c3da3a76f2249dcf239d8878f
Guilherme Luis <guile.hm@hotmail.com>
2018-08-16 22:06:32 -0300

    Add rest framework documentation

--- controller/urls.py
+++ controller/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     path('bank/', include('bank.urls', namespace='bank')),
     path('api/', include('API.urls', namespace='api')),
+    path('docs/', include_docs_urls(title='ECS')),
 ]
