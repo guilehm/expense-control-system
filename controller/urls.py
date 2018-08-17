52bc28fcea4bc5bd81c90013bc71e23547a31da7
Guilherme Luis <guile.hm@hotmail.com>
2018-08-16 22:15:06 -0300

    Configure Swagger

--- controller/urls.py
+++ controller/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework.documentation import include_docs_urls
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ECS",
+      default_version='v1',
+      description="Expense Control System",
+      contact=openapi.Contact(email="guile.hm@hotmail.com"),
+   ),
+   public=True,
+   permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
