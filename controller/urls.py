071a47468766e00a52a20014da3e604d3a5cd3e4
Guilherme Luis <guile.hm@hotmail.com>
2018-08-18 19:23:15 -0300

    Fix imports

--- controller/urls.py
+++ controller/urls.py
@@ -17,10 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
 from rest_framework.permissions import AllowAny
-
-from rest_framework import routers
 
 from API import views as api_views
 
