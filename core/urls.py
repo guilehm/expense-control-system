0cf3c7cbe31ce5b6a41c4dc7996b00cfc88783ef
Guilehm <guile.hm@hotmail.com>
2018-08-19 12:49:46 -0300

    Create html url

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.index, name='home'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
