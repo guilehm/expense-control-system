9ba739098134105a78761ed10ca28c7fad5da513
Guilherme Luis <guile.hm@hotmail.com>
2018-08-22 21:03:22 -0300

    Add logging settings

--- controller/settings.py
+++ controller/settings.py
@@ -217,3 +217,36 @@ AUTHENTICATION_BACKENDS = (
 LOGIN_REDIRECT_URL = '/home/'
 
 INTERNAL_IPS = ['127.0.0.1', ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
