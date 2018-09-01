e373904219e5f5e308ab8799d3d40fc245d8c5a0
Guilherme Luis <guile.hm@hotmail.com>
2018-08-31 23:47:40 -0300

    Use decorator for admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from core.models import Category, Tag
+from core.models import Category, CSV, Tag
 
 
 # Register your models here.
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('title', 'owner')
     list_filter = ('owner', 'date_added',)
@@ -12,13 +13,10 @@ class TagAdmin(admin.ModelAdmin):
     }
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'owner')
     list_filter = ('owner', 'date_added',)
     prepopulated_fields = {
         'slug': ('title',)
     }
-
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Tag, TagAdmin)
