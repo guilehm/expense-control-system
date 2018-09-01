18e3ae8eeb0605f105c9a796d05586542878a90c
Guilherme Luis <guile.hm@hotmail.com>
2018-09-01 10:09:59 -0300

    Generate logs for category importer

--- utils/category_importer.py
+++ utils/category_importer.py
@@ -31,10 +31,11 @@ def process_csv_category_file(instance):
         category, created = Category.objects.get_or_create(
             owner=instance.owner,
             title=title,
-            slug=slug,
-            description=description
         )
         if created:
+            category.slug = slug
+            category.description = description
+            category.save()
             logger.info(
                 'Category #{id} {title} created for "{user}".'.format(
                     id=category.id,
@@ -44,8 +45,31 @@ def process_csv_category_file(instance):
             )
         else:
             logger.warning(
-                'Category #{title} already existed for user "{user}".'.format(
+                'Category #{id} {title} already existed for user "{user}".'.format(
+                    id=category.id,
                     title=category.title,
                     user=instance.owner.username,
                 )
             )
+            if category.slug != slug:
+                logger.info(
+                    'Updating category #{id} {title} slug from "{old_slug}" to "{new_slug}".'.format(
+                        id=category.id,
+                        title=category.title,
+                        old_slug=category.slug,
+                        new_slug=slug,
+                    )
+                )
+            category.slug = slug
+            if category.description != description:
+                logger.info(
+                    'Updating category #{id} {title} description '
+                    'from "{old_description}" to "{new_description}".'.format(
+                        id=category.id,
+                        title=category.title,
+                        old_description=category.description,
+                        new_description=description,
+                    )
+                )
+            category.description = description
+            category.save()
